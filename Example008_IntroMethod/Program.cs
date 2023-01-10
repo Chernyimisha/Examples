576e6418abfcc31352426e45d4c1c3e534f93e2c
Proshkin Mikhail <chernyimisha@rambler.ru>
2023-01-11 01:01:17 +0300

    Поработал на 3 лекции

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -10,11 +10,11 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 2311;
+int b2 = 156;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
-int c3 = 33;
+int c3 = 56;
 
 /*int max = a1;
 if (b1 > max ) max = b1;
@@ -26,10 +26,10 @@ if (a3 > max ) max = a3;
 if (b3 > max ) max = b3;
 if (c3 > max ) max = c3;*/
 
-//int max1 = Max (a1, b1, c1);
-//int max2 = Max (a2, b2, c2);
-//int max3 = Max (a3, b3, c3);
-//int max = Max (max1, max2, max3);
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max (max1, max2, max3);
 
 int max = Max(
     Max (a1, b1, c1),
--- Example010_MethodArrey/Program.cs
+++ Example010_MethodArrey/Program.cs
@@ -1,6 +1,6 @@
-﻿int [] array = {1, 12, 31, 4, 15, 16, 4, 18};
+﻿int [] array = {1, 12, 31, 4, 18, 16, 4, 18};
 int n = array.Length;
-int find = 4;
+int find = 18;
 int index = 0;
 while (index < n)
 {
