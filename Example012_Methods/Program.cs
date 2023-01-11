0745302617e869ae903992652cfbeaee5d4abd20
Proshkin Mikhail <chernyimisha@rambler.ru>
2023-01-11 15:19:51 +0300

    Разобраны методы по лекции 3

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -84,7 +84,7 @@ Console.WriteLine(res);
 // прусского короля. Вы так красноречивы. Вы
 // дадите мне чаю?
 
-string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+/*string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
              + "ежели бы вас послали вместо нашего милого"
             + "Винценгероде, вы бы взяли приступом согласие"
             + "прусского короля. Вы так красноречивы. Вы"
@@ -98,22 +98,56 @@ string Replace(string text, char oldValue, char newValue)
     string result = String.Empty;
 
     int length = text.Length;
-    for (int i = 0; i<length; i++)
+    for (int i = 0; i < length; i++)
     {
-        if(text[i] == oldValue) result = result + $"{newValue}";
+        if (text[i] == oldValue) result = result + $"{newValue}";
         else result = result + $"{text[i]}";
     }
     return result;
 }
 
-string newText = Replace(text, ' ' , '|');
+string newText = Replace(text, ' ', '|');
 Console.WriteLine(newText);
-newText = Replace(newText, 'к' , 'К');
-Console.WriteLine(newText);
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);*/
 
+// 1. Найти позицию минимального элемента
+// в неотсортированной части массива
+// 2. Произвести обмен этого значения со
+// значением первой неотсортированной позиции
+// 3. Повторять пока есть не отсортированные
+// элементы
 
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
+void SelectionSort(int[] array)
+
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+    int temporary = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
 
 
 
